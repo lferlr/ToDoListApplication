122d54e0d4166ed32aa69a4325d442d88a4644d0
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-02-05 13:54:39 -0300

    Criando as Views e DB com migrations

--- Data/Migrations/20220205160711_CreateTodoTable.Designer.cs
+++ Data/Migrations/20220205160711_CreateTodoTable.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyTodoApp.Data;
 
 namespace MyTodoApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220205160711_CreateTodoTable")]
+    partial class CreateTodoTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
